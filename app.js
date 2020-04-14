e769ade94e08ee46050fb3e327850998bf6c27fd
Solano <solanoalvarez21@gmail.com>
2020-04-14 06:09:32 -0600

    Template literales

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// Codigo de JavaScript
+var nombre = "Juan";
+var apellido = "Perez";
+var edad = 32;
+//let texto = "Hola, " + nombre + " " + apellido + "("+ edad +")";
+var texto = "Hola, " + nombre + " " + apellido + " (" + edad + ")";
+var texto2 = "" + (1 + 2);
+console.log(texto2);
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-// Un archivo simple de TypeScript
+let nombre:string = "Juan";
+let apellido:string = "Perez";
+let edad:number = 32;
+
+//let texto = "Hola, " + nombre + " " + apellido + "("+ edad +")";
+
+let texto = `Hola, ${nombre} ${apellido} (${edad})`;
+
+let texto2:string = `${1+2}`;
+
+console.log(texto2);
+
+
+
